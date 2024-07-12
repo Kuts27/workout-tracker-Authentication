84290849766b0828e509cd59cbc9c996a3549a38
Sachin Kute <sachinkute2@gmail.com>
2024-07-12 22:41:21 +0530

    Login and Signup pages added

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,13 +3,19 @@ import { useEffect, useState } from "react"
 // components
 import WorkoutDetails from "../components/WorkoutDetails"
 import WorkoutForm from "../components/WorkoutForm"
+import { useAuthContext } from "../hooks/useAuthContext"
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null)
+  const {user} = useAuthContext()
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts')
+      const response = await fetch('/api/workouts',{
+        headers: {
+          'Authorization': `Bearer ${user.token}`
+        }
+      })
       const json = await response.json()
 
       if (response.ok) {
@@ -17,8 +23,11 @@ const Home = () => {
       }
     }
 
-    fetchWorkouts()
-  }, [workouts])
+    if(user) {
+      fetchWorkouts()
+    }
+    
+  }, [workouts, user])
 
   return (
     <div className="home">
